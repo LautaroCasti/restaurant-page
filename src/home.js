8648305bc7a870876cc50a2c595c81c05b2b1127
Lautaro Ignacio Castillo <castillolautaroig@gmail.com>
2023-12-22 21:52:15 -0300

    Change home view

--- src/home.js
+++ src/home.js
@@ -1,10 +1,15 @@
 export default function home(parentNode) {
+  const homeDiv = document.createElement("div");
+  homeDiv.classList.add("home");
+
   const h1 = document.createElement("h1")
-  h1.classList.add("main__title")
+  h1.classList.add("home__title")
 
   const btn = document.createElement("button");
-  btn.classList.add("button");
+  btn.classList.add("button home__btn");
 
-  parentNode.appendChild(h1);
-  parentNode.appendChild(btn);
+  homeDiv.appendChild(h1);
+  homeDiv.appendChild(btn);
+
+  parentNode.appendChild(homeDiv)
 }
